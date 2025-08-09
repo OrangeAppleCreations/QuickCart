c4c74b282a5fdb64a6ae089fadb132a63e63dadb
OrangeApple Creations <j.mulderije@outlook.com>
2025-08-09 08:19:35 +0200

    Initial Commit

---
+++ QuickCart/QuickCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCartApp.swift
+//  QuickCart
+//
+//  Created by OrangeApple Creations on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuickCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
